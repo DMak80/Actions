a4271ca265a656f32bf9404d8642c22e3d935a82
malyshkin <malyshkin@hightech.group>
2022-05-07 14:19:01 +0300

    add hw 2 tests and blank class

--- 2k/HomeWork2/Hw2Tests/UnitTest1.cs
+++ 2k/HomeWork2/Hw2Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-using Hw2;
+using Homework2.IL;
 using Xunit;
 
 namespace Hw2Tests;
@@ -6,10 +6,30 @@ namespace Hw2Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Calculate_220Plus8_228Returned()
     {
-        var expected = 5;
-        var result = Program.GetFive();
-        Assert.Equal(expected, result);
+        var res = Calculator.Calculate(220, Operations.Plus, 8);
+        Assert.Equal(228, res);
+    }
+
+    [Fact]
+    public void Calculate_1401Minus64_1337Returned()
+    {
+        var res = Calculator.Calculate(1401, Operations.Minus, 64);
+        Assert.Equal(1337, res);
+    }
+
+    [Fact]
+    public void Calculate_4Multiply372_1488Returned()
+    {
+        var res = Calculator.Calculate(4, Operations.Multiply, 372);
+        Assert.Equal(1488, res);
+    }
+
+    [Fact]
+    public void Calculate_359Divide4_89dot75Returned()
+    {
+        var res = Calculator.Calculate(359, Operations.Divide, 4);
+        Assert.Equal(89.75, res, 6);
     }
 }
