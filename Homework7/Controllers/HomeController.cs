2d9d1fda04d4e122121dbdc714721b2293916aca
MaratElagin <maratelagin@gmail.com>
2022-07-05 12:30:56 +0300

    HW7 - Reflection complete

---
+++ Homework7/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using HomeWork7.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UserProfile(UserProfile profile)
+        {
+            return View(profile);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ Homework7/Models/UserProfile.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeWork7.Models;
+
+public enum Sex
+{
+    Male,
+    Female
+};
+
+public class UserProfile
+{
+    [Required(ErrorMessage = "Fill this field!")]
+    [MaxLength(30, ErrorMessage = "First Name must be less than 30 symbols")]
+    //[Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Fill this field!")]
+    [MaxLength(30, ErrorMessage = "Last Name must be less than 30 symbols")]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Fill this field!")]
+    [MaxLength(30, ErrorMessage = "Middle Name must be less than 30 symbols")]
+    [Display(Name = "Middle name")]
+    public string? MiddleName { get; set; }
+
+    [Required(ErrorMessage = "Fill this field!")]
+    [Range(10, 100, ErrorMessage = "Your age must be in the range of 10 to 100")]
+    [Display(Name = "Age")]
+    public int Age { get; set; }
+
+    public Sex Sex { get; set; }
+}
