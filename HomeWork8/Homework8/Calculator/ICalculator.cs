2bd31819aea589ae2fccf0bc6b1ad362a3c026f5
MaratElagin <maratelagin@gmail.com>
2022-07-12 16:03:56 +0300

    add hw8 with tests

---
+++ HomeWork8/Homework8/Calculator/ICalculator.cs
@@ -0,0 +1,9 @@
+namespace Homework8.Calculator;
+
+public interface ICalculator
+{
+    string Plus(double val1, double val2);
+    string Minus(double val1, double val2);
+    string Multiply(double val1, double val2);
+    string Divide(double firstValue, double secondValue);
+}
---
+++ HomeWork8/Homework8/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+using System.Globalization;
+using Homework8.Calculator;
+using Homework8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework8.Controllers;
+
+public class CalculatorController : Controller
+{
+    public string Calculate([FromServices] ICalculator calculator,
+        string val1,
+        string operation,
+        string val2)
+    {
+        var val1IsDouble = double.TryParse(val1, NumberStyles.Number, CultureInfo.InvariantCulture,  out var v1);
+        var val2IsDouble = double.TryParse(val2, NumberStyles.Number, CultureInfo.InvariantCulture,  out var v2);
+        if (!val1IsDouble || !val2IsDouble)
+            return $"Введите числа"; 
+
+        var isOperation = Enum.TryParse<Operation>(operation, true, out var op);
+        if (!isOperation)
+            return "Недопустимая операция. Допустимые: plus, minus, multiply и divide";
+            
+        return op switch
+        {
+            Operation.Plus => calculator.Plus(v1, v2),
+            Operation.Minus => calculator.Minus(v1, v2),
+            Operation.Multiply => calculator.Multiply(v1, v2),
+            Operation.Divide => calculator.Divide(v1, v2),
+        };
+    }
+        
+    public IActionResult Index()
+    {
+        return Content(
+            "Заполните val1, operation(plus, minus, multiply, divide) и val2 здесь '/calculator/calculate?val1= &operation= &val2= '\n" +
+            "и добавьте её в адресную строку.");
+    }
+}
