0965770f56bf39a7a30c7de880bb2f7aa0e9bb21
TimurSadorov <t.sadorov@ext.hightech.group>
2022-07-14 13:13:42 +0300

    change for coverage

--- HomeWork9/HomeWork9/Services/MathCalculator/Validator/MathExpressionValidator.cs
+++ HomeWork9/HomeWork9/Services/MathCalculator/Validator/MathExpressionValidator.cs
@@ -55,7 +55,7 @@ public class MathExpressionValidator: IMathExpressionValidator
             lastToken = currentToken;
         }
 
-        if (lastToken is not null && lastToken.Value.Type == TokenType.Operation)
+        if (lastToken?.Type == TokenType.Operation)
         {
             errorMessage = "An expression cannot end with an operation sign";
             return false;
