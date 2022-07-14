f82335f5f22e096c42df664c234fbb4ba1d3c64f
TimurSadorov <t.sadorov@ext.hightech.group>
2022-07-14 13:40:17 +0300

    transfer change from hw 9 and delete all hw except hw 10

--- HomeWork9/HomeWork9/Services/MathCalculator/IMathCalculatorService.cs
+++
@@ -1,8 +0,0 @@
-using HomeWork9.Dto;
-
-namespace HomeWork9.Services.MathCalculator;
-
-public interface IMathCalculatorService
-{
-    public Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression);
-}
