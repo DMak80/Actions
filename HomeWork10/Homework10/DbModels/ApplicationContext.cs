eae7cb27b02130f2d1cf1e7a40bcd73097a9a2bb
TimurSadorov <t.sadorov@ext.hightech.group>
2022-07-18 13:08:09 +0300

    add template for hw 10

--- HomeWork10/Homework10/DbModels/ApplicationContext.cs
+++ HomeWork10/Homework10/DbModels/ApplicationContext.cs
@@ -4,8 +4,6 @@ namespace Homework10.DbModels;
 
 public class ApplicationContext: DbContext
 {
-	public DbSet<SolvingExpression> SolvingExpressions => Set<SolvingExpression>();
-
 	public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
 	{
 	}
--- HomeWork10/Homework10/Program.cs
+++ HomeWork10/Homework10/Program.cs
@@ -1,18 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
-using Homework10.Configuration;
-using Homework10.DbModels;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
-builder.Services
-    .AddMathCalculator()
-    .AddCachedMathCalculator();
-
-builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
